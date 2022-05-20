b3b76c668aabc0ba107714385cb3bf4aa6ee748a
Pengyu Wang <hncswpy@gmail.com>
2022-05-19 18:20:50 -0700

    Completed MapReduce:

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,18 +1,113 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"strconv"
+	"sync"
+)
 
+type TaskType int
+type TaskStatus int
+
+// TaskType Initialization
+const (
+	MapTask TaskType = iota
+	ReduceTask
+	NoTask
+	ExitTask
+)
+
+// TaskStatus Intialization
+const (
+	NotStarted TaskStatus = iota
+	Running
+	Completed
+	Failed
+)
+
+// TaskInfo for keeping track of task status and comm to worker through RPC
+type TaskInfo struct {
+	Type     TaskType
+	Status   TaskStatus
+	FilePath string
+	WorkerId int
+	TaskId   int
+	NMap     int
+	NReduce  int
+}
 
 type Coordinator struct {
 	// Your definitions here.
+	inputFiles  []string
+	nReduce     int
+	nReduceLeft int
+	taskArray   []TaskInfo
+	mu          sync.Mutex
+	nMap        int
+	nMapLeft    int
+}
 
+func (c *Coordinator) FindNextTask() *TaskInfo {
+	if c.nMapLeft > 0 {
+		for i := 0; i < c.nMap; i++ {
+			if c.taskArray[i].Status == NotStarted && c.taskArray[i].Type == MapTask {
+				return &c.taskArray[i]
+			}
+		}
+		// nMapLeft decremented when task finishes, will panic when all tasks are assigned but not finished
+		// panic("Cannot find available map task")
+	} else if c.nReduceLeft > 0 {
+		for i := c.nMap; i < len(c.taskArray); i++ {
+			if c.taskArray[i].Status == NotStarted && c.taskArray[i].Type == ReduceTask {
+				return &c.taskArray[i]
+			}
+		}
+		// panic("Cannot find available reduce task")
+	} else {
+		// Assuming no tasks fail, return exit task when no task available
+		exitTask := TaskInfo{ExitTask, NotStarted, "", -1, -1, c.nMap, c.nReduce}
+		return &exitTask
+	}
+	noTask := TaskInfo{NoTask, NotStarted, "", -1, -1, c.nMap, c.nReduce}
+	return &noTask
 }
 
 // Your code here -- RPC handlers for the worker to call.
+
+func (c *Coordinator) AssignTask(args *RequestTaskArgs, reply *RequestTaskResponse) error {
+	workerId := args.WorkerId
+
+	c.mu.Lock()
+
+	newTask := c.FindNextTask()
+	c.mu.Unlock()
+
+	newTask.WorkerId = workerId
+	newTask.Status = Running
+
+	reply.Task = newTask
+	// PrintTaskInfo(reply.Task)
+	return nil
+}
+
+func (c *Coordinator) ReportTask(args *ReportTaskArgs, reply *ReportTaskReply) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if args.Status == Failed {
+		c.taskArray[args.TaskId].Status = NotStarted
+	} else if args.Status == Completed {
+		if args.Type == MapTask {
+			c.nMapLeft -= 1
+		} else if args.Type == ReduceTask {
+			c.nReduceLeft -= 1
+		}
+	}
+	return nil
+}
 
 //
 // an example RPC handler.
@@ -23,7 +118,6 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -49,7 +143,7 @@ func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
-
+	ret = c.nMapLeft == 0 && c.nReduceLeft == 0
 
 	return ret
 }
@@ -63,7 +157,29 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
+	c.inputFiles = files
+	c.nReduce = nReduce
+	c.nReduceLeft = c.nReduce
+	c.nMap = len(files)
+	c.nMapLeft = c.nMap
+	c.taskArray = make([]TaskInfo, 0, c.nMap+c.nReduce)
 
+	log.Printf("[Coordinator] Coordinator created with %d map tasks and %d reduce tasks.\n", c.nMap, c.nReduce)
+
+	var MakeTaskInfo func(TaskType, TaskStatus, string, int) *TaskInfo
+	MakeTaskInfo = func(tt TaskType, ts TaskStatus, fp string, taskId int) *TaskInfo {
+		return &TaskInfo{tt, ts, fp, -1, taskId, c.nMap, c.nReduce}
+	}
+
+	for i := 0; i < c.nMap; i++ {
+		mapTask := MakeTaskInfo(MapTask, NotStarted, files[i], i)
+		c.taskArray = append(c.taskArray, *mapTask)
+	}
+
+	for i := 0; i < c.nReduce; i++ {
+		reduceTask := MakeTaskInfo(ReduceTask, NotStarted, strconv.Itoa(i), i+c.nMap)
+		c.taskArray = append(c.taskArray, *reduceTask)
+	}
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,16 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"strconv"
+	"strings"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -12,6 +18,13 @@ import "hash/fnv"
 type KeyValue struct {
 	Key   string
 	Value string
+}
+
+type worker struct {
+	workerId int
+	mapf     func(string, string) []KeyValue
+	reducef  func(string, []string) string
+	task     *TaskInfo
 }
 
 //
@@ -24,7 +37,6 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
@@ -32,10 +44,152 @@ func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
-
+	w := worker{workerId: os.Getpid(), mapf: mapf, reducef: reducef}
+	for {
+		w.requestTask()
+	}
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
+}
+
+func PrintTaskInfo(ti *TaskInfo) {
+	fmt.Printf("Task Type: %v\tStatus: %v\nWorker id: %d\tTask id: %d\nNMap: %d\tNReduce: %d\n", ti.Type, ti.Status, ti.WorkerId, ti.TaskId, ti.NMap, ti.NReduce)
+}
+
+func (w *worker) requestTask() {
+	reqArg := RequestTaskArgs{w.workerId}
+	reqReply := RequestTaskResponse{}
+	call("Coordinator.AssignTask", &reqArg, &reqReply)
+	// PrintTaskInfo(reqReply.Task)
+	w.task = reqReply.Task
+	// fmt.Printf("NReduce: %d\n", reqReply.Task.NReduce)
+	log.Printf("[Worker %d] Requested a task from master\n", w.task.WorkerId)
+	if w.task.Type == MapTask {
+		w.executeMapTask()
+	} else if w.task.Type == ReduceTask {
+		w.executeReduceTask()
+	} else if w.task.Type == ExitTask {
+		os.Exit(0)
+	}
+}
+
+func (w *worker) handleError(err error) {
+	w.task.Status = Failed
+	log.Printf("[Worker %d] Error: %v", w.workerId, err)
+	w.reportTask()
+	// Exit the worker process
+	os.Exit(1)
+}
+
+func (w *worker) reportTask() {
+	args := ReportTaskArgs{w.task.Status, w.workerId, w.task.TaskId, w.task.Type}
+	var reply ReportTaskReply
+	call("Coordinator.ReportTask", &args, &reply)
+}
+
+func (w *worker) executeMapTask() {
+	mapTask := w.task
+	if mapTask.Type != MapTask {
+		log.Panic("Wrong task type given.")
+	}
+	var outputKV []KeyValue
+
+	log.Printf("[Worker %d] Executing map task on %s\n", mapTask.WorkerId, mapTask.FilePath)
+
+	content, err := ioutil.ReadFile(mapTask.FilePath)
+	if err != nil {
+		w.handleError(err)
+	}
+
+	outputKV = w.mapf(mapTask.FilePath, string(content))
+
+	reduceBins := make(map[int][]KeyValue)
+	// Hashing into different reduce bins
+	for _, KV := range outputKV {
+		key := KV.Key
+		hashedKey := ihash(key) % mapTask.NReduce
+		if _, ok := reduceBins[hashedKey]; !ok {
+			reduceBins[hashedKey] = make([]KeyValue, len(outputKV)/mapTask.NReduce)
+		}
+		reduceBins[hashedKey] = append(reduceBins[hashedKey], KV)
+	}
+
+	var outFilePath string
+	for reduceId, KVpairs := range reduceBins {
+		outFilePath = fmt.Sprintf("mr-%d-%d", mapTask.TaskId, reduceId)
+		file, err := os.Create(outFilePath)
+		if err != nil {
+			w.handleError(err)
+		}
+		enc := json.NewEncoder(file)
+		for _, kv := range KVpairs {
+			err := enc.Encode(kv)
+			if err != nil {
+				w.handleError(err)
+			}
+		}
+	}
+
+	w.task.Status = Completed
+	w.reportTask()
+}
+
+func (w *worker) executeReduceTask() {
+	// Need to read in multiple files
+	reduceTask := w.task
+	if reduceTask.Type != ReduceTask {
+		log.Panic("Wrong task type given.")
+	}
+
+	log.Printf("[Worker %d] Executing reduce task on %s", reduceTask.WorkerId, reduceTask.FilePath)
+
+	assignedReduceNum, err := strconv.Atoi(reduceTask.FilePath)
+	if err != nil {
+		w.handleError(err)
+	}
+
+	kvmap := make(map[string][]string)
+	for i := 0; i < reduceTask.NMap; i++ {
+		filepath := fmt.Sprintf("mr-%d-%d", i, assignedReduceNum)
+		file, err := os.Open(filepath)
+		if err != nil {
+			w.handleError(err)
+		}
+
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			if _, ok := kvmap[kv.Key]; !ok {
+				kvmap[kv.Key] = make([]string, 0, reduceTask.NMap)
+			}
+			kvmap[kv.Key] = append(kvmap[kv.Key], kv.Value)
+		}
+	}
+
+	// Iterating the kvmap to call reducef once for each key-values pair
+	res := make([]string, 0)
+	for key, values := range kvmap {
+		keyAgg := w.reducef(key, values)
+		res = append(res, fmt.Sprintf("%v %v", key, keyAgg))
+	}
+
+	outFile := fmt.Sprintf("mr-out-%d", assignedReduceNum)
+	err = ioutil.WriteFile(outFile, []byte(strings.Join(res, "\n")), 0600)
+	if err != nil {
+		w.handleError(err)
+	}
+	w.task.Status = Completed
+	w.reportTask()
+}
+
+func printKeyValue(kv []KeyValue) {
+	for i := 0; i < len(kv); i++ {
+		fmt.Printf("%s\t%s\n", kv[i].Key, kv[i].Value)
+	}
 }
 
 //
